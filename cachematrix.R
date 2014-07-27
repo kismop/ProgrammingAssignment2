545ad899a9348993eb376438a9f510328e63a80e
kismop <faby2010perez@gmail.com>
2014-07-27 12:14:31 -0500

    cambios actualizados

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+## it contain 2 functios that create a matrix and get its inverse
+## makeCacheMatrix declare the values of matrix vector
 
-## Write a short comment describing this function
+## It Create a matrix, before you must declare the values of vector, row and colummun
+##Ejm declare vector c   c<-c(1,3,4,5) and  after makeCacheMatrix(c, 2, 2)
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(c, nrow, ncol) {
+  x=matrix(c, nrow, ncol)
+  x
+  
 }
 
 
-## Write a short comment describing this function
+## CacheSolve get a inverse matrix that you declare in the makeCache function
+## Ejm x is a matrix that you declare before , then you must cacheSolve(x) and get inversa matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  ## Return a matrix that is the inverse of 'x'
+  inverse<-solve(x)
+  inverse
 }
